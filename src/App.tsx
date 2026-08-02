a8b2afd3f5179f875f93e3bdcb5f1f79b2198466
Ariti Chawla <aritichawla2110@gmail.com>
2026-08-02 13:45:34 +0530

    Refactor App component structure and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import { lazy, Suspense } from "react";
-import "./App.css";
+import React from 'react';
+import Contact from './components/Contact';
+import Skills from './components/Skills';
+import Education from './components/Education';
+import Certifications from './components/Certifications';
+import About from './components/About';
+import Work from './components/Work';
+import Career from './components/Career';
+import TechStack from './components/TechStack';
+import './styles/ATS-Friendly.css';
+import './App.css';
 
-const CharacterModel = lazy(() => import("./components/Character"));
-const MainContainer = lazy(() => import("./components/MainContainer"));
-import { LoadingProvider } from "./context/LoadingProvider";
-
-const App = () => {
+function App() {
   return (
-    <>
-      <LoadingProvider>
-        <Suspense>
-          <MainContainer>
-            <Suspense>
-              <CharacterModel />
-            </Suspense>
-          </MainContainer>
-        </Suspense>
-      </LoadingProvider>
-    </>
+    <main>
+      {/* ATS-FRIENDLY SECTIONS */}
+      
+      {/* 1. Contact & Header Section */}
+      <Contact />
+
+      {/* 2. About Section */}
+      <About />
+
+      {/* 3. Skills Section */}
+      <Skills />
+
+      {/* 4. Work/Projects Section */}
+      <Work />
+
+      {/* 5. Career/Experience Section */}
+      <Career />
+
+      {/* 6. Education Section */}
+      <Education />
+
+      {/* 7. Tech Stack Section */}
+      <TechStack />
+
+      {/* 8. Certifications & Awards Section */}
+      <Certifications />
+    </main>
   );
-};
+}
 
 export default App;
